601b0900fb1f130af250f8cc3e52f609ca56c742
foridislam332 <foridislam991@gmail.com>
2023-08-03 07:59:08 +0600

    footer done

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
-        <footer className="container">
-            footer coming
+        <footer className="container bg-dark">
+            <div className="p-12">
+                <p className="text-white text-xl md:text-2xl text-center font-medium">© 2021. All rights reserved by MD Forid Hossain</p>
+            </div>
         </footer>
     );
 };
